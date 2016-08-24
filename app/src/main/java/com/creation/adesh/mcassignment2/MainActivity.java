406984acdc3e6f161d0ca9fa8d234a58623ba537
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2016-08-25 00:53:00 +0530

    initialized with last assignment

--- app/src/main/java/com/creation/adesh/mcassignment2/MainActivity.java
+++ app/src/main/java/com/creation/adesh/mcassignment2/MainActivity.java
@@ -2,12 +2,54 @@ package com.creation.adesh.mcassignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static TextView mQuestionText = null;
+    private Integer number = null;
+    private Question presentQuestion = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mQuestionText = (TextView) findViewById(R.id.questionText);
+        if(savedInstanceState == null) {
+            presentQuestion= new Question();
+            number = presentQuestion.getNumber();
+        }
+        else
+        {
+            number=savedInstanceState.getInt("number");
+            presentQuestion = new Question(number);
+        }
+
+        mQuestionText.setText(number+" is a prime number.");
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("number",number);
+    }
+    public void answerTrue(View view){
+        if(presentQuestion.checkAnswer(true)==true)
+            Toast.makeText(getApplicationContext(),"Correct Response!",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(getApplicationContext(),"Incorrect Response!",Toast.LENGTH_SHORT).show();
+    }
+    public void answerFalse(View view){
+        if(presentQuestion.checkAnswer(false)==true)
+            Toast.makeText(getApplicationContext(),"Correct Response!",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(getApplicationContext(),"Incorrect Response!",Toast.LENGTH_SHORT).show();
+    }
+    public void next(View view){
+        presentQuestion= new Question();
+        number = presentQuestion.getNumber();
+        mQuestionText.setText(number+" is a prime number.");
     }
 }
---
+++ app/src/main/java/com/creation/adesh/mcassignment2/Question.java
@@ -0,0 +1,35 @@
+package com.creation.adesh.mcassignment2;
+
+import java.util.Random;
+
+/**
+ * Created by adesh on 8/14/16.
+ */
+public class Question {
+    Integer number = 0;
+    Boolean answer = true;
+    private void setAnswer(){
+        for(int i=2;i<number;i++)
+            if(number%i==0)
+                answer=false;
+    }
+    public Question(){
+        number = new Random().nextInt(1000)+1;
+        setAnswer();
+    }
+
+    public Question(Integer number){
+        this.number = number;
+        setAnswer();
+    }
+
+    Integer getNumber(){
+        return number;
+    }
+    Boolean checkAnswer(Boolean givenAnswer){
+        if(givenAnswer == answer)
+            return true;
+        else
+            return false;
+    }
+}
